6f5a82a66b462f3056729bc0a8c49ae400026eec
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-09-27 20:27:42 -0300

    feat: button component, update profile page

---
+++ src/components/Button/styles.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.button`
+  background: #00BEBB;
+  height: 56px;
+  border-radius: 10px;
+  border: 0;
+  padding: 0 16px;
+  color: #012C50;
+  width: 100%;
+  font-weight: 500;
+  margin-top: 16px;
+  transition: background-color 0.2s;
+  &:hover {
+    background: ${shade(0.1, '#00BEBB')};
+  }
+`;
