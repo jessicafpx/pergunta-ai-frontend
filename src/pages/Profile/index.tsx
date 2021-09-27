1719b5a357c38429b941496ac1b7ba5b9e44c898
Lauro <lauromvs@hotmail.com>
2021-09-27 19:26:37 -0300

    feat: add login, register and profile html structure

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,7 +2,48 @@ import React from 'react';
 
 const Profile = () => {
   return (
-    <h1>Profile</h1>
+    <>
+      <figure>
+        <img src="" alt="" />
+        <figcaption>Avatar</figcaption>
+        <button type="button">Editar avatar</button>
+      </figure>
+      <h1>Meu perfil</h1>
+      <button type="button">Editar</button>
+      <form>
+        <fieldset>
+          <legend>
+            Nome
+          </legend>
+          <input type="text"/>
+        </fieldset>
+        <fieldset>
+          <legend>
+            Curso
+          </legend>
+          <input type="text"/>
+        </fieldset>
+        <fieldset>
+          <legend>
+            E-mail
+          </legend>
+          <input type="text"/>
+        </fieldset>
+        <fieldset>
+          <legend>
+            Data de nascimento
+          </legend>
+          <input type="text"/>
+        </fieldset>
+        <fieldset>
+          <legend>
+            Senha
+          </legend>
+          <input type="text"/>
+        </fieldset>
+        <button type="submit"> Confirmar alterações</button>
+      </form>
+    </>
   );
 };
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,14 @@ import { Switch, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Login from '../pages/Login';
 import Profile from '../pages/Profile';
+import SignUp from '../pages/SignUp';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
     <Route path="/login" exact component={Login} />
     <Route path="/profile" exact component={Profile} />
+    <Route path="/signup" exact component={SignUp} />
   </Switch>  
 );
 
