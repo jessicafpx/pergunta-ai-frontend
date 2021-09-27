aa65ae9e70eb4bacb2c3de039f3d3ff9575cd368
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-09-27 20:04:51 -0300

    feat: global setup

---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  background-color: #012C50;
+  height: 145px;
+
+  img {
+    width: 190px;
+    margin-top: 50px;
+  }
+`;
