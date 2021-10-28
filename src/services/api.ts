8f957b383202cb03748e122bfbb11554a994e53f
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-10-28 09:01:47 -0300

    feat: add proxy

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,8 @@ const api = axios.create({
   httpsAgent: new https.Agent({
     rejectUnauthorized: false
   }),
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "http://localhost:3000/",
 });
+
 
 export default api;
