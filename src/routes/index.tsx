1b568d8e5b69ee57720d8042c410824632d40262
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-10-28 22:04:04 -0300

    feat: add new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-
 import { Switch, Route } from 'react-router-dom';
 
-import Dashboard from '../pages/Dashboard';
 import Login from '../pages/Login';
 import Profile from '../pages/Profile';
 import SignUp from '../pages/SignUp';
+import ForgotPassWord from '../pages/ForgotPassword';
+import SetPassword from '../pages/SetPassword';
 
 const Routes = () => (
   <Switch>
@@ -13,6 +12,8 @@ const Routes = () => (
     <Route path="/login" exact component={Login} />
     <Route path="/profile" exact component={Profile} />
     <Route path="/signup" exact component={SignUp} />
+    <Route path="/forgot-password" exact component={ForgotPassWord} />
+    <Route path="/set-password" exact component={SetPassword} />
   </Switch>
 );
 
