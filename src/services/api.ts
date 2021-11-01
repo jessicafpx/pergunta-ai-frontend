e369248c9463ecbf43e6e0f9ccce7f1e13d72a4f
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-11-01 15:21:50 -0300

    test: add headers

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 import https from 'https';
 
+const headers = {
+  'Content-Type': 'application/json',
+};
+
 const api = axios.create({
   httpsAgent: new https.Agent({
     rejectUnauthorized: false
   }),
   baseURL: process.env.REACT_APP_API_URL,
+  headers
 });
 
 export default api;
