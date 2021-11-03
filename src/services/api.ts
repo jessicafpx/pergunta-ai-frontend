b3e3e3066af7c9fc5eb04f042de37bcc80149c35
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-11-03 19:20:38 -0300

    refactor: remove proxy and req header

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,11 @@
 import axios from 'axios';
 import https from 'https';
 
-const headers = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*'
-};
-
 const api = axios.create({
   httpsAgent: new https.Agent({
     rejectUnauthorized: false
   }),
   baseURL: process.env.REACT_APP_API_URL,
-  headers
 });
 
 export default api;
