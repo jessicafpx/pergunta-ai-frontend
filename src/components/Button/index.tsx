edc886c48d0ea0278691d5dd7252a5474b0ef228
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-11-04 22:00:19 -0300

    feat: update button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,10 +4,11 @@ import { Container } from './styles';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   loading?: boolean;
+  isDisabled?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
-  <Container type="button" {...rest}>
+const Button: React.FC<ButtonProps> = ({ children, loading, isDisabled, ...rest }) => (
+  <Container disabled={isDisabled} isDisabled={isDisabled} type="button" {...rest}>
     {loading ? 'Carregando...' : children}
   </Container>
 );
