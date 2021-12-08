5bb675586a99eac5bff4b0327c10e003e9f2df1d
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-12-08 20:26:17 -0300

    feat: add topic details edit flow

--- src/components/NoTopics/index.tsx
+++ src/components/NoTopics/index.tsx
@@ -5,12 +5,19 @@ import noTopicsImg from '../../assets/no-topics.png';
 
 import { Wrapper } from './styles';
 
+type NoTopicsProps = {
+  isAnswer?: boolean;
+}
 
-const NoTopics: React.FC = () => {
+const NoTopics: React.FC <NoTopicsProps>= ({isAnswer}) => {
   return (
     <Wrapper>
       <img src={noTopicsImg} alt="no topics" className="no-topics-img"/>
-      <p>Nenhuma pergunta por aqui...</p>
+      {isAnswer ? (
+        <p>Nenhuma resposta por aqui...</p>
+      ) : (
+        <p>Nenhuma pergunta por aqui...</p>
+      )}
     </Wrapper>
   );
 }
